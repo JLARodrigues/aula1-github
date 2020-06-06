6da545c11ba16cd550655421bbc0899de5885961
JLARrodrigues <x2rodrigues@gmail.com>
2020-06-06 12:21:43 -0300

    Mensagem de boa noite

--- src/app/Programa.java
+++ src/app/Programa.java
@@ -2,10 +2,9 @@ package app;
 
 public class Programa {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		System.out.println("Bom dia!");
 		System.out.println("Boa tarde!");
+		System.out.println("Boa noite!");
 	}
-
 }
